69905c02d63a2a629e07d60e0e52634ca0a31236
maoxiaoke <qiangqiang.mao@ele.me>
2018-08-11 19:21:48 +0800

    add demo

---
+++ modeltest/src/components/Input.vue
@@ -0,0 +1,15 @@
+<template>
+  <input
+    :value="value"
+    @input="$emit('input', $event.target.value)" />
+</template>
+
+<script>
+export default {
+  name: 'MyInput',
+  props: ['value']
+}
+</script>
+
+<style scoped>
+</style>
---
+++ modeltest/src/components/index.js
@@ -0,0 +1,4 @@
+import Input from './Input.vue'
+import Vue from 'vue'
+
+export default Vue.component(Input.name, Input)
